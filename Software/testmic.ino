c5265187f01b739ffb9720008c7ce43c61ea6bee
Technocracy <josiasmoukpe@gmail.com>
2018-05-22 15:27:28 -0400

    Update testmic.ino

--- Software/testmic.ino
+++ Software/testmic.ino
@@ -10,14 +10,13 @@
 // is called.
 // It queries the state of that pin, and sets the onboard LED to reflect that
 // pin's state.
-void soundISR()
-{
-int pin_val;
-pin_val = digitalRead(PIN_GATE_IN);
-digitalWrite(PIN_LED_OUT, pin_val);
+void soundISR() {
+  int pin_val;
+  pin_val = digitalRead(PIN_GATE_IN);
+  digitalWrite(PIN_LED_OUT, pin_val);
 }
-void setup()
-{
+
+void setup() {
 Serial.begin(9600);
 // Configure LED pin as output
 pinMode(PIN_LED_OUT, OUTPUT);
@@ -27,25 +26,24 @@ attachInterrupt(IRQ_GATE_IN, soundISR, CHANGE);
 // Display status
 Serial.println("Initialized");
 }
-void loop()
-{
+
+void loop() {
 int value;
 // Check the envelope input
 value = analogRead(PIN_ANALOG_IN);
 // Convert envelope value into a message
 Serial.print("Status: ");
-if(value <= 10)
-{
-Serial.println("Quiet.");
-}
-else if( (value > 10) && ( value <= 30) )
-{
-Serial.println("Moderate.");
-}
-else if(value > 30)
-{
-Serial.println("Loud.");
-}
+  
+  if(value <= 10) {
+    Serial.println("Quiet.");
+  }
+  else if( (value > 10) && ( value <= 30) ) {
+    Serial.println("Moderate.");
+  }
+  else if(value > 30) {
+    Serial.println("Loud.");
+  }
+  
 // pause for 1 second
-delay(1000);
+//delay(1000);
 }
