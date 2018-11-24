4a04092dd719f901f72a0356968f419cbc69d46f
Technocracy <josiasmoukpe@gmail.com>
2018-11-24 16:16:28 -0500

    Update bt_response.ino

--- Speech_Recognition/Arduino/bt_response.ino
+++ Speech_Recognition/Arduino/bt_response.ino
@@ -1,11 +1,17 @@
 
+
 #include<SoftwareSerial.h>
+#include<Servo.h>
 
 #define TxD 3
 #define RxD 2
 #define LED_PIN 7
+#define HOME_POS 0
+#define ON 90
+#define OFF 180
 
-SoftwareSerial bluetoothSerial(TxD, RxD);
+SoftwareSerial bluetoothSerial(TxD, RxD); //seetting bluetooth transmission protocol on Serial
+Servo flick; //actuator for light
 
 char c;
 
@@ -13,6 +19,8 @@ void setup() {
   bluetoothSerial.begin(9600);
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
+  flick.attach(9);
+  flick.write(HOME_POS);
   }
 
 void loop() {
@@ -20,10 +28,12 @@ void loop() {
     c = bluetoothSerial.read();
     Serial.println(c);
     if(c=='1'){
-      digitalWrite(LED_PIN, HIGH);          
+      digitalWrite(LED_PIN, HIGH); 
+      flick.write(ON);         
     }
     if(c=='0'){
       digitalWrite(LED_PIN, LOW);
+      flick.write(OFF);
     }
   }
 }
