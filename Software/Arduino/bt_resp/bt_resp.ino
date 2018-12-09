a0ab485df0bd457cbd2790e002b4db34084a967b
ERUD1T3 <josiasmoukpe@gmail.com>
2018-12-09 16:49:26 -0500

    made_changes_to_support_mega

--- Software/Arduino/bt_resp/bt_resp.ino
+++ Software/Arduino/bt_resp/bt_resp.ino
@@ -1,4 +1,5 @@
-//Design to run on Arduino Mega 2560
+//Designed to run on Arduino Mega 2560
+#include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
@@ -9,18 +10,32 @@
 #define ON 90
 #define OFF 180
 #define SERVO 4
+#define BAUD 9600
+//LCD definitions
+#define EN 23
+#define RS 25
+#define D4 28
+#define D5 26
+#define D6 24
+#define D7 22
+
 
 SoftwareSerial bluetoothSerial(TxD, RxD); //seetting bluetooth transmission protocol on Serial
 Servo flick; //actuator for light
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 char bt_data;
+void lcd_print_cmd(String cmd,uint8_t line = 1);
 
 void setup() {
-  bluetoothSerial.begin(9600);
-  Serial.begin(9600);
+  lcd.begin(16, 2);
+  bluetoothSerial.begin(BAUD);
+  Serial.begin(BAUD);
   pinMode(LED_PIN, OUTPUT);
   flick.attach(SERVO);
   //flick.write(HOME_POS);
+  lcd.setCursor(0, 0);
+  lcd.print("Voice Command:");
   }
 
 void loop() {
@@ -28,12 +43,23 @@ void loop() {
     bt_data = bluetoothSerial.read();
     Serial.println(bt_data);
     if(bt_data=='1'){
+        lcd_print_cmd("ON");
       digitalWrite(LED_PIN, HIGH); 
       flick.write(ON);         
     }
     if(bt_data=='0'){
+        lcd_print_cmd("OFF");
       digitalWrite(LED_PIN, LOW);
       flick.write(OFF);
     }
   }
+}
+
+
+void lcd_print_cmd(String cmd, uint8_t line) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Voice Command:");
+    lcd.setCursor(0, line);
+    lcd.print(cmd);
 }
