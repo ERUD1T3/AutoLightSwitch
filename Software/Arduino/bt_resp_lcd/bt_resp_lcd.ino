384deac08258059e9bdafd6d5cf91947bf0ae537
ERUD1T3 <josiasmoukpe@gmail.com>
2018-12-09 19:03:26 -0500

    never_do_last_min_changes

--- Software/Arduino/bt_resp_lcd/bt_resp_lcd.ino
+++ Software/Arduino/bt_resp_lcd/bt_resp_lcd.ino
@@ -1,16 +1,18 @@
 //Designed to run on Arduino Mega 2560
-#include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
+//#include <LiquidCrystal.h>
 #include <Servo.h>
 
 #define TxD 3
 #define RxD 2
-#define LED_PIN 27
+#define LED_PIN 7
 #define HOME_POS 0
 #define ON 90
 #define OFF 180
-#define SERVO 4
-#define BAUD 9600
+#define SERVO 9
+#define BAUD 115200
+
+
 //LCD definitions
 #define EN 23
 #define RS 25
@@ -22,20 +24,20 @@
 
 SoftwareSerial bluetoothSerial(TxD, RxD); //seetting bluetooth transmission protocol on Serial
 Servo flick; //actuator for light
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+//LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 char bt_data;
-void lcd_print_cmd(String cmd,uint8_t line = 1);
+//void lcd_print_cmd(String cmd,uint8_t line = 1);
 
 void setup() {
-  lcd.begin(16, 2);
-  bluetoothSerial.begin(BAUD);
-  Serial.begin(BAUD);
+  //lcd.begin(16, 2);
+  bluetoothSerial.begin(9600);
+  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   flick.attach(SERVO);
   //flick.write(HOME_POS);
-  lcd.setCursor(0, 0);
-  lcd.print("Voice Command:");
+  //lcd.setCursor(0, 0);
+  //lcd.print("Voice Command:");
   }
 
 void loop() {
@@ -43,19 +45,19 @@ void loop() {
     bt_data = bluetoothSerial.read();
     Serial.println(bt_data);
     if(bt_data=='1'){
-        lcd_print_cmd("ON");
+        //lcd_print_cmd("ON");
       digitalWrite(LED_PIN, HIGH); 
       flick.write(ON);         
     }
     if(bt_data=='0'){
-        lcd_print_cmd("OFF");
+        //lcd_print_cmd("OFF");
       digitalWrite(LED_PIN, LOW);
       flick.write(OFF);
     }
   }
 }
 
-
+/*
 void lcd_print_cmd(String cmd, uint8_t line) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -63,3 +65,4 @@ void lcd_print_cmd(String cmd, uint8_t line) {
     lcd.setCursor(0, line);
     lcd.print(cmd);
 }
+*/
--- Software/Arduino/bt_response/bt_response.ino
+++ Software/Arduino/bt_response/bt_response.ino
@@ -1,14 +1,15 @@
-//Design to run on Arduino Mega 2560
+//Design to run on Arduino Uno
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
 #define TxD 3
 #define RxD 2
-#define LED_PIN 27
+#define LED_PIN 7
 #define HOME_POS 0
 #define ON 90
 #define OFF 180
-#define SERVO 4
+#define SERVO 9
+#define BAUD 9600
 
 SoftwareSerial bluetoothSerial(TxD, RxD); //seetting bluetooth transmission protocol on Serial
 Servo flick; //actuator for light
@@ -16,8 +17,8 @@ Servo flick; //actuator for light
 char bt_data;
 
 void setup() {
-  bluetoothSerial.begin(9600);
-  Serial.begin(9600);
+  bluetoothSerial.begin(BAUD);
+  Serial.begin(BAUD);
   pinMode(LED_PIN, OUTPUT);
   flick.attach(SERVO);
   //flick.write(HOME_POS);
@@ -26,7 +27,7 @@ void setup() {
 void loop() {
   if(bluetoothSerial.available()){
     bt_data = bluetoothSerial.read();
-    Serial.println(bt_data);
+    //Serial.println(bt_data);
     if(bt_data=='1'){
       digitalWrite(LED_PIN, HIGH); 
       flick.write(ON);         
